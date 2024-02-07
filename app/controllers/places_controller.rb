90b1f6998e22586aa6c779ec2c20933d06579f46
Cyril26 <cyrilodm26@gmail.com>
2024-02-07 13:28:55 +0000

    add places endpoint to show data we need on screen

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  def index
+  def index # return HTML
     render "pages/places/index"
   end
 end
--- app/javascript/components/PlacesList.jsx
+++ app/javascript/components/PlacesList.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 
-class PlacesList extends React.Component {
-  render() {
+function PlacesList() {
+  const [ loading, setLoading ] = useState(true)
+  const [ loadedPlaces, setLoadedPlaces ] = useState([])
+
+  useEffect(() => {
+    const apiEndpoint = "api/places"
+    fetch(apiEndpoint)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setLoadedPlaces(data.places)
+        setLoading(false)
+      })
+  }, [])
+  
+  if (loading) {
     return (
-      <div>Places List rendered in react!</div>
+      <div>Loading...</div>
+    )
+  } else {
+    return (
+      <>
+        {
+          loadedPlaces.map((place, index) => (
+            <div key={index}>
+              <table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>City</th>
+                  <th>Recent Upload Speed</th>
+                  <th>Recent Upload Speed Units</th>
+                  <th>Number of measurements</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{place.name}</td>
+                  <td>{place.city}</td>
+                  <td>{place.most_recent_download_speed}</td>
+                  <td>{place.most_recent_download_speed_units}</td>
+                  <td>{place.number_of_measurements}</td>
+                </tr>
+              </tbody>
+              </table>
+            </div>    
+          ))
+        }
+      </>
     )
   }
 }
