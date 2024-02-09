28ed7eed6a26c7555f44e5c3ca127087d886e7dd
Cyril26 <cyrilodm26@gmail.com>
2024-02-09 13:40:22 +0000

    add form to collect (pure css/html, no functionality yet)

--- app/javascript/components/NewInternetSpeed.jsx
+++ app/javascript/components/NewInternetSpeed.jsx
@@ -1,13 +1,48 @@
 import React from "react"
 
-export default function NewInternetSpeed () {
+export default function NewInternetSpeed() {
   return (
     <div className="bg-white p-8 rounded-md w-full">
       <div className=" flex items-center justify-between pb-6">
         <div>
-          <h2 className="text-4xl text-gray-600 font-semibold">Create a new </h2>
+          <h2 className="text-4xl text-gray-600 font-semibold">Log Internet Speed</h2>
         </div>
       </div>
-  </div>
+
+      <div className="md:ml-2 w-96
+      ">
+        <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="placeName">
+          Place Name
+        </label>
+        <input
+          className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+          id="placeName"
+          type="text"
+          placeholder="Place Name"
+        />
+      </div>
+      <div className="md:ml-2 mt-2 w-96
+      ">
+        <label className="block mb-2 text-sm font-bold text-gray-700" htmlFor="city">
+          City
+        </label>
+        <input
+          className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+          id="City"
+          type="text"
+          placeholder="City"
+        />
+      </div>
+
+      <div className="md:ml-2 mt-4 w-96
+       text-center">
+        <button
+          className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+          type="button"
+        >
+          Register Account
+        </button>
+      </div>
+    </div>
   )
 }
